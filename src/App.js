8acec245bc4b047b12de64945c758bc5d8585dee
Carl (Gyeongsik Park) <tpnet3@gmail.com>
2021-12-14 13:34:50 +0900

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import jwt from "jsonwebtoken";
 
 function App() {
+  const [loggedData, setLoggedData] = useState();
+
+  const onClickLogin = async () => {
+    // 프로젝트 아이디로 수정하세요.
+    const projectId = "ccc15289-9634-4cbe-bd2a-449d539a8cdc";
+    const redirectUri = window.location.protocol + "//" + window.location.host;
+
+    const queryString = new URLSearchParams({
+      client_id: projectId,
+      redirect_uri: redirectUri,
+    }).toString();
+
+    const loginUrl = `https://mrlogin.io/login?${queryString}`;
+
+    // provider 를 지정하면 원하는 소셜로그인 화면을 다이렉트로 표시할 수 있습니다.
+    // const loginUrl = `https://mrlogin.io/auth/${provider}/login?${queryString}`;
+
+    window.location.href = loginUrl;
+  };
+
+  useEffect(() => {
+    let parsedUrl = new URL(window.location.href);
+    const accessToken = parsedUrl.searchParams.get("access_token");
+    const refreshToken = parsedUrl.searchParams.get("refresh_token");
+
+    if (accessToken || refreshToken) {
+      setLoggedData({
+        accessToken: jwt.decode(accessToken),
+        refreshToken: refreshToken,
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button className="loginButton" onClick={onClickLogin}>
+        로그인 버튼
+      </button>
+      <pre className="jwtPre">{JSON.stringify(loggedData, null, 2)}</pre>
     </div>
   );
 }
